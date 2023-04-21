4b9f37ccfa03bea115db66a152e2eba90aa43517
ChefMike <dinis.palha@gmail.com>
2023-04-21 11:04:51 +0100

    first commit

---
+++ src/token/service.ts
@@ -0,0 +1,13 @@
+
+
+export const tokenService = async (id: number) => {
+
+    // verify token exists in nft collection
+
+    // check if nft image is already cached
+
+    // if not, generate nft image and cache it
+
+    // return nft json data
+
+}
