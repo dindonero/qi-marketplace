bf41b68779949d6a3675867e5d26063cfba2dbe0
Inês Lacerda <ines.lacerda@sap.com>
2023-05-29 19:45:51 +0100

    revert change

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 require('dotenv').config();
 
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'http://localhost:3000/:path*',
-          },
-        ]
-      },
 }
 
 module.exports = nextConfig
