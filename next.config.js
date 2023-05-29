0e02faa50dcaf2a5e3449a3187d3f793cfe365c0
Inês Lacerda <ines.lacerda@sap.com>
2023-05-29 19:39:38 +0100

    testing netlify

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 require('dotenv').config();
 
 const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'http://localhost:3000/:path*',
+          },
+        ]
+      },
 }
 
 module.exports = nextConfig
--- src/nftMetadata/fetchMetadata.ts
+++ src/nftMetadata/fetchMetadata.ts
@@ -2,10 +2,7 @@ export const requestNFTMetadataBackend = async (tokenIds: number[]) => {
     let metadatas: { [key: number]: Promise<Response> } = {};
 
     for (const tokenId of tokenIds)
-        metadatas[tokenId] = fetch(`http://localhost:3000/api/token/${tokenId}`,{
-            mode: 'no-cors',
-            method: "get",
-           })
+        metadatas[tokenId] = fetch(`http://localhost:3000/api/token/${tokenId}`)
 
     return metadatas
 }
