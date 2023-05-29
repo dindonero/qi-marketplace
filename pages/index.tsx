ca776b87325461b036da8d733749a7e5db1cb551
Inês Lacerda <ines.lacerda@sap.com>
2023-05-29 23:05:39 +0100

    Add background image to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
 import {NextPage} from "next";
 import {useMoralis} from "react-moralis";
+import Image from "next/image";
 
 const Home: NextPage = () => {
+    const backgroundImage = "/images/home9.jpg"; // Path to your image file
 
     return (
-        <div className="container mx-auto">
-            <h1 className="py-4 px-4 font-bold text-2xl">Yiqi NFTs Marketplace</h1>
+        <div style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "top",
+            backgroundRepeat: "no-repeat",
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+          }}
+        >
+            <h1 className="font-bold text-5xl">Yiqi NFTs Marketplace</h1>
+            {/* <Image src="/images/home.jpg" alt="Description of the image" width={100%} height={100vh}/> */}
         </div>
     );
 }
