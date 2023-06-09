f8c0c9cd2fc80b5bace00de0f12c168db13e4a89
ChefMike <dinis.palha@gmail.com>
2023-06-09 15:42:45 +0100

    added opensea button missing styling

--- pages/NFTDisplay.tsx
+++ pages/NFTDisplay.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image"
 import {Card} from "web3uikit"
 import {useEffect, useState} from "react";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
 import BurnModal from "../components/BurnModal";
 import ChangeBackgroundModal from "../components/ChangeBackgroundModal";
 import {Button, useDisclosure} from "@chakra-ui/react";
+import OpenseaButton from "../components/OpenseaButton";
 
 export default function NFTDisplay() {
-    const { isOpen, onOpen, onClose } = useDisclosure();
+    const {isOpen, onOpen, onClose} = useDisclosure();
     const [isOpenBurnModal, SetisOpenBurnModal] = useState<boolean>(true)
 
     const router = useRouter();
@@ -27,23 +28,27 @@ export default function NFTDisplay() {
             justifyContent: "center",
             alignItems: "center",
             color: "white",
-          }}
-        >   
+        }}
+        >
             <Button onClick={() => {
                 SetisOpenBurnModal(true);
                 onOpen();
-                }}>Burn</Button>
-            <div >
-                {isOpenBurnModal ? <BurnModal isOpen={isOpen} onClose={onClose} tokenId={String(tokenId)} /> : null }
+            }}>Burn</Button>
+            <div>
+                {isOpenBurnModal ? <BurnModal isOpen={isOpen} onClose={onClose} tokenId={String(tokenId)}/> : null}
             </div>
             <Button onClick={() => {
                 SetisOpenBurnModal(false);
                 onOpen();
-                }}>Change Background</Button>
-            {isOpenBurnModal ? 
-                <BurnModal isOpen={isOpen} onClose={onClose} tokenId={String(tokenId)} /> 
-                : <ChangeBackgroundModal isOpen={isOpen} onClose={onClose} tokenId={String(tokenId)} />
+            }}>Change Background</Button>
+            {isOpenBurnModal ?
+                <BurnModal isOpen={isOpen} onClose={onClose} tokenId={String(tokenId)}/>
+                : <ChangeBackgroundModal isOpen={isOpen} onClose={onClose} tokenId={String(tokenId)}/>
             }
+            { /* tokenId!.toString() hack todo improve this*/}
+            tokenId ?
+            (<OpenseaButton tokenId={tokenId!.toString()} isBackground={false}/>) :
+            (<div></div>)
             <h1 className="font-bold text-5xl">#{tokenId}</h1>
         </div>
     )
