842cd55008c093637d0522bd1ad74585898201bb
Inês Lacerda <ines.lacerda@sap.com>
2023-06-19 22:35:07 +0100

    mint flip animation

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,13 +1,47 @@
-import { MintButton } from "../components/MintButton";
-import {NextPage} from "next";
+import { useState, useEffect } from 'react';
+import styles from './Mint.module.css'; // Import CSS file for styling
+import Image from 'next/image';
 
-const Mint: NextPage = () => {
+const Mint = () => {
+    const path = "/images/mintLoading/";
+    
+    const [isFlipped, setIsFlipped] = useState(false);
+    var index : number = 2;
+    const [frontImage, setFrontImage] = useState(path + '0.png');
+    const [backImage, setBackImage] = useState(path + '1.png');
+
+    const swapImages = () => {
+        setFrontImage(path + index.toString() + '.png');
+        index += 1;
+        // setBackImage(path + index.toString() + '.png');
+        if (index > 17) index = 0;
+        setIsFlipped(false);
+    };
+
+    useEffect(() => {
+        const flipInterval = setInterval(() => {
+            setIsFlipped(true);
+            setTimeout(() => {
+                swapImages();
+            }, 1000); // Wait for 1 second before flipping back
+          }, 2000); // Change card flip interval (in milliseconds)
+          return () => clearInterval(flipInterval);
+      }, []);
+
     return (
-        <div>
-            <h1 className="py-4 px-4 font-bold text-2xl">Mint a Yiqi</h1>
-            <MintButton/>
+        <div className={styles.flipContainer} >
+            <div className={`${styles.flipCard} ${isFlipped ? styles.flipped : ''}`}>
+            <div className={styles.flipCardInner}>
+                <div className={styles.flipCardFront}>
+                    <Image src={frontImage} alt="Front Image" width={300} height={300} />
+                </div>
+                <div className={styles.flipCardBack}>
+                    <h1>Loading...</h1>
+                </div>
+            </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Mint
+export default Mint;
